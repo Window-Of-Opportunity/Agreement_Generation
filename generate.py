87dc8459f99aced535b77e8bcb87bb9505b58541
Jeffrey <jeffrey.s.wu@sjsu.edu>
2020-10-12 14:09:21 -0700

    Flatten form feature implementation added.

--- generate.py
+++ generate.py
@@ -1,6 +1,5 @@
-from collections import OrderedDict
 from PyPDF2 import PdfFileWriter, PdfFileReader
-from PyPDF2.generic import BooleanObject, NameObject, IndirectObject
+from PyPDF2.generic import BooleanObject, NameObject, IndirectObject, NumberObject
 import datetime
 
 #Sample form values, in dictionary format
@@ -20,16 +19,16 @@ FORM_VALUES = {
   "Job Site Address": "Job Site Address",
   "Month": datetime.date.today().month,
   "Year": datetime.date.today().year,
-  "Digital Signature": "Digital Signature",
+#   "Digital Signature": "Digital Signature",
   "Permit": True,
-  "Right To Cancel": True,
+#   "Right To Cancel": True,
   "Progress Payment": "Progress Payment",
   "Down Payment": "$0",
   "Window-Door" : "W-D"
 }
 
 # Modification of the pdfwriter in order to be up to current pdf spec, or else form will not update properly upon generation.
-def update_form_properly_writer(writer: PdfFileWriter):
+def updateFormProperlyWriter(writer: PdfFileWriter):
     try:
         catalog = writer._root_object
         if "/AcroForm" not in catalog:
@@ -41,7 +40,7 @@ def update_form_properly_writer(writer: PdfFileWriter):
         return writer
 
     except Exception as e:
-        print('set_need_appearances_writer() catch : ', repr(e))
+        print('updateFormProperlyWriter() catch : ', repr(e))
         return writer
 
 # Method to update check boxes, WORK IN PROGRESS.
@@ -55,27 +54,61 @@ def update_form_properly_writer(writer: PdfFileWriter):
 #                     NameObject("/AS"): NameObject(fields[field])
 #                 })
 
-# Program that reads and fills pdf form, generating an output pdf file with form filled.
-# requires designated agreement file, read readme file for instructions to obtain file.
-if __name__ == '__main__':
-
-    infile = "2020 Agreement Marvin.pdf"
-    outfile = "out.pdf"
-
+def generateFlattenedPdf(infile, outfile):
     pdf = PdfFileReader(open(infile, "rb"), strict=False)
     if "/AcroForm" in pdf.trailer["/Root"]:
         pdf.trailer["/Root"]["/AcroForm"].update(
             {NameObject("/NeedAppearances"): BooleanObject(True)})
 
     pdf2 = PdfFileWriter()
-    update_form_properly_writer(pdf2)
+    updateFormProperlyWriter(pdf2)
     if "/AcroForm" in pdf2._root_object:
         pdf2._root_object["/AcroForm"].update(
             {NameObject("/NeedAppearances"): BooleanObject(True)})
 
 
     pdf2.addPage(pdf.getPage(0))
+    # Update form values.
     pdf2.updatePageFormFieldValues(pdf2.getPage(0), FORM_VALUES)
 
+    # Flatten form fields.
+    flat_page = pdf2.getPage(0)
+    for j in range(0, len(flat_page['/Annots'])):
+        writer_annot = flat_page['/Annots'][j].getObject()
+        for field in FORM_VALUES: 
+            if writer_annot.get('/T') == field:
+                writer_annot.update({
+                    NameObject("/Ff"): NumberObject(1)   # make ReadOnly
+                })
+    
     outputStream = open(outfile, "wb")
     pdf2.write(outputStream)
+
+def generatePdf(infile, outfile):
+    pdf = PdfFileReader(open(infile, "rb"), strict=False)
+    if "/AcroForm" in pdf.trailer["/Root"]:
+        pdf.trailer["/Root"]["/AcroForm"].update(
+            {NameObject("/NeedAppearances"): BooleanObject(True)})
+
+    pdf2 = PdfFileWriter()
+    updateFormProperlyWriter(pdf2)
+    if "/AcroForm" in pdf2._root_object:
+        pdf2._root_object["/AcroForm"].update(
+            {NameObject("/NeedAppearances"): BooleanObject(True)})
+
+
+    pdf2.addPage(pdf.getPage(0))
+    # Update form values.
+    pdf2.updatePageFormFieldValues(pdf2.getPage(0), FORM_VALUES)
+    
+    outputStream = open(outfile, "wb")
+    pdf2.write(outputStream)
+
+# Program that reads and fills pdf form, generating an output pdf file with form filled.
+# requires designated agreement file, read readme file for instructions to obtain file.
+if __name__ == '__main__':
+
+    infile = "2020 Agreement Marvin.pdf"
+
+    # generateFlattenedPdf(infile, "Flattened-out.pdf")
+    # generatePdf(infile, "out.pdf")
